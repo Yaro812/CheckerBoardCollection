f3af16d8bef185b92e36002b1d307d041d91706b
Thorax <thorax@me.com>
2016-12-22 12:34:24 +0300

    Initial Commit

---
+++ CheckerBoardCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckerBoardCollection
+//
+//  Created by Yaroslav Pasternak on 22/12/2016.
+//  Copyright © 2016 Yaroslav Pasternak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
